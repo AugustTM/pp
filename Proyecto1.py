946269db487adaed7b8fde4cbdcd0881854bed04
AugustTM <eatrillerasm@gmail.com>
2023-04-23 19:53:49 -0500

    se añadio proyecto.py

---
+++ Proyecto1.py
@@ -0,0 +1,18 @@
+def sum(a,b):
+    return a+b
+
+print(sum(10,20))
+
+# una nueva funcion
+
+def mult(x,y):
+    return x*y
+
+print(mult(10.01, 100))
+print(mult(7.21, 44))
+
+# una segunda nueva funcion
+
+def div(x,y):
+    return x/y
+
